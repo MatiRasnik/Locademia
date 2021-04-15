536c7b00bddd71f6edb8b5a4e7f326d3a4afe0ac
JuaniiAndresM <thewolfmodzyt@gmail.com>
2021-04-15 17:34:00 -0300

    TipoCoche + Autos

--- Form/TipoCoche.php
+++ Form/TipoCoche.php
@@ -1,6 +1,12 @@
 <?php
     $tipo = $_POST['tipo'];
-    $modelo = isset($_POST['modelo']) ? $_POST['modelo']: "Up" ;
+    if($_POST['tipo'] == "Sedán"){
+        $modelo = isset($_POST['modelo']) ? $_POST['modelo']: "Versa" ;
+    }else if($_POST['tipo'] == "Hatchback"){
+        $modelo = isset($_POST['modelo']) ? $_POST['modelo']: "Up" ;
+    }else if($_POST['tipo'] == "Mini"){
+        $modelo = isset($_POST['modelo']) ? $_POST['modelo']: "Up" ;
+    }
     $tipocoche = "  <div class='tipo-wrapper-grid'>
                         <div class='tipo-wrapper'>
                             <h2>Seleccionar Modelo del Vehiculo:</h2>
@@ -8,11 +14,11 @@
                             <select name='autos' id='autos' onChange=tipoAuto('$tipo');>";
     
     switch($tipo){
-        case "Sedan":
-            $tipocoche .=  "<option value='volvo'>Volvo</option>
-                            <option value='saab'>Saab</option>
-                            <option value='opel'>Opel</option>
-                            <option value='audi'>Audi</option>";
+        case "Sedán":
+            $tipocoche .=  "<option value='Versa'>Nissan Versa 2018</option>
+                            <option value='ModelS'>Tesla Model S</option>
+                            <option value='Lancer'>Mitsubishi Lancer</option>
+                            <option value='Civic'>Honda Civic</option>";
             break;
         
         case "Hatchback":
@@ -37,6 +43,27 @@
                         <div class='foto-vehiculo'>";
 
     switch($modelo){
+
+        //SEDAN
+
+        case "Versa":
+            $tipocoche .=  "<img src='../img/Versa.png' alt='' />";
+            break;
+        
+        case "ModelS":
+            $tipocoche .=  "<img src='../img/ModelS.png' alt='' />";
+            break;
+        
+        case "Lancer":
+            $tipocoche .=  "<img src='../img/Lancer.png' alt='' />";
+            break;
+
+        case "Civic":
+            $tipocoche .=  "<img src='../img/Civic.png' alt='' />";
+            break; 
+
+        //HATCHBACK
+
         case "Up":
             $tipocoche .=  "<img src='../img/Up.png' alt='' />";
             break;
