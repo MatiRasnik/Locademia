7f300d44ecda1c42568ee4182866c7cff8094e1d
SvenQuiring <svenpqiep@gmail.com>
2021-04-17 19:57:52 -0300

    Grayout.

--- Form/Reservas.php
+++ Form/Reservas.php
@@ -5,7 +5,7 @@ echo "<div class='Titulo'> <h1>Vespertino</h1> </div>";
 
 for($i=14;$i<=19;$i++) {
     if(date("H", mktime($i)) < $hora) {
-        echo "<label class='horario'> <input type='checkbox' id='horario1' name='horario1' value=" . date('H:i', mktime($i, 00)) . "> <label>" . date('H:i', mktime($i, 00)) . " hs </label> </label>";
+        echo "<label class='horario' style='background-color: lightgrey;'> <input type='checkbox' id='horario1' name='horario1' value=" . date('H:i', mktime($i, 00)) . "> <label>" . date('H:i', mktime($i, 00)) . " hs </label> </label>";
     } else {
         echo "<label class='horario'> <input type='checkbox' id='horario1' name='horario1' value=" . date('H:i', mktime($i, 00)) . "> <label>" . date('H:i', mktime($i, 00)) . " hs </label> </label>";
     }
