923629bc71f8fa0eb35f960ca661464006a1f446
ElTomy <tomassosa1303@gmail.com>
2021-04-17 22:52:28 -0300

    no quiero nada con la vida

--- js/Register.js
+++ js/Register.js
@@ -8,9 +8,9 @@ function Register1(){
         data: { ci: ci },
         success: function(respuesta) {
             if(respuesta == 1){
-                Cedula = ci;
-                    alert("Su cedula a sido validada");
-                    window.location.assign("Register2.html")
+                sessionStorage.setItem('Cedula', ci);
+                alert("Su cedula a sido validada");
+                window.location.assign("Register2.html")
             }else{
                 alert("Su cedula es invalida");
             }
@@ -21,15 +21,11 @@ function Register2(){
     var username = $('#usuario').val();
     var password = $('#password').val();
     var password2 = $('#password2').val();
-    alert(Cedula);
-    alert(username);
-    alert(password);
-    alert(password2);
     if(password == password2){
         $.ajax({
             url: "Register.php",
             type: "post",
-            data: {CI: Cedula, Usename: username, Password: password},
+            data: {CI: sessionStorage.getItem('Cedula'), Usename: username, Password: password},
             success: function(respuesta) {
                 if(respuesta == 1){
                     alert("se guardaron sus datos");
