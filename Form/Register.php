2639250d37384bad59a7612f242169f6915d8047
ElTomy <tomassosa1303@gmail.com>
2021-04-17 21:41:28 -0300

    mi sanidad mental necesita que esto funke

--- Form/Register.php
+++ Form/Register.php
@@ -1,32 +1,29 @@
 <?php
 include 'conexion.php';
+if(isset($_POST['CI']) && isset($_POST['Usename']) && isset($_POST['Password'])){
+    $sql = "SELECT * FROM usuario WHERE Username = '" . $_POST['Usename'] . "'";
+    $result = $mysqli->query($sql);
+    $ss = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    if (isset($ss['Username'])) {
+        $respuesta ="4";
+    } else {
+        $usuario = '"' . $mysqli->real_escape_string($_POST['Usename']) . '"';
+        $contra = '"' . $mysqli->real_escape_string($_POST['Password']) . '"';
+        $CI = '"' . $mysqli->real_escape_string($_POST['CI']) . '"';
+        $insert_row = $mysqli->query("INSERT INTO usuario  (Username, Pass, CI) VALUES($usuario, $contra, $CI)");
+        $respuesta = "3";
+    }
+}else{
     if(isset($_POST['ci'])){
         $sql = "SELECT * FROM cliente WHERE CI = '" . $_POST['ci'] . "'";
         $result = $mysqli->query($sql);
         $ss = mysqli_fetch_array($result, MYSQLI_ASSOC);
         if (isset($ss['CI'])) {
-            $asd = "1";
+            $respuesta = "1";
         } else {
-        $asd = "2";
-        }
-    }else{
-        if(isset($_POST['CI']) && isset($_POST['Usename']) && isset($_POST['Password'])){
-            $sql = "SELECT * FROM usuario WHERE Username = '" . $_POST['Usename'] . "'";
-            $result = $mysqli->query($sql);
-            $ss = mysqli_fetch_array($result, MYSQLI_ASSOC);
-            if (isset($ss['Usuario'])) {
-                $asd ="4";
-            } else {
-                $usuario = '"' . $mysqli->real_escape_string($_POST['Usename']) . '"';
-                $contra = '"' . $mysqli->real_escape_string($_POST['Password']) . '"';
-                $CI = '"' . $mysqli->real_escape_string($_POST['CI']) . '"';
-                $insert_row = $mysqli->query("INSERT INTO usuario  (Username, Pass, CI) VALUES($usuario, $contra, $CI)");
-                $asd = "3";
-            }
-        }else{
-            $asd = "4";
+        $respuesta = "2";
         }
     }
-    echo $asd;
-    return $asd;
+}
+return $respuesta;
 ?>
--- js/Register.js
+++ js/Register.js
@@ -6,24 +6,13 @@ function Register1(){
         url: "Register.php",
         type: "post",
         data: { ci: ci },
-        success: function(asd) {
-            alert(asd);
-            switch(asd){
-                case '1':
-                    Cedula = ci;
+        success: function(respuesta) {
+            if(respuesta == 1){
+                Cedula = ci;
                     alert("Su cedula a sido validada");
                     window.location.assign("Register2.html")
-                break;
-                case '2':
-                    alert("Su cedula es invalidada");
-                break;
-                case '3':
-                    alert("se guardaron sus datos");
-                    window.location.assign("Login.html")
-                break;
-                case '4':
-                    alert("este usuario ya existe");
-                break;
+            }else{
+                alert("Su cedula es invalidada");
             }
         },
     });
@@ -32,12 +21,19 @@ function Register2(){
     var username = $('#usuario').val();
     var password = $('#password').val();
     var password2 = $('#password2').val();
+    alert(Cedula);
     if(password == password2){
         $.ajax({
             url: "Register.php",
             type: "post",
             data: {CI: Cedula, Usename: username, Password: username},
-            success: function() {
+            success: function(respuesta) {
+                if(respuesta == 1){
+                    alert("se guardaron sus datos");
+                    window.location.assign("Login.html")
+                }else{
+                    alert("este usuario ya existe");
+                }
             },
         });
     }else{
