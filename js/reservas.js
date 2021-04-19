0dedce80b6b9080b2e3417151dd83086dc59afd5
SvenQuiring <svenpqiep@gmail.com>
2021-04-19 17:52:40 -0300

    Algunos cambios.

--- js/reservas.js
+++ js/reservas.js
@@ -1,5 +1,17 @@
-$( document ).ready(function(){
 
+
+function revisarHoras(dia) {
+    $.ajax({
+        url: "Reservas.php",
+        type: "post",
+        data: { dia:dia },
+        success: function (html) {
+          document.getElementById(".col2").innerHTML = html;
+        },
+    });
+}
+
+
+$( document ).ready(function() {
     $(".col2").load('Reservas.php');
-    console.log("Hola");
 });
