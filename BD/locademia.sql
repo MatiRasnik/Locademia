d3d5859bab037e1e21fdf00a4e81ba405646b0a7
MatiRasnik <MatiRasnik@users.noreply.github.com>
2021-04-20 12:41:13 -0300

    bd update

--- BD/locademia.sql
+++ BD/locademia.sql
@@ -57,6 +57,7 @@ CREATE TABLE `automoviles` (
   `matricula` varchar(50) NOT NULL,
   `tipo` varchar(50) NOT NULL,
   `descripcion` varchar(50) NOT NULL,
+  `foto` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`matricula`),
   UNIQUE KEY `CI` (`CI`),
   CONSTRAINT `automoviles_ibfk_1` FOREIGN KEY (`CI`) REFERENCES `chofer` (`CI`)
@@ -69,7 +70,7 @@ CREATE TABLE `automoviles` (
 
 LOCK TABLES `automoviles` WRITE;
 /*!40000 ALTER TABLE `automoviles` DISABLE KEYS */;
-INSERT INTO `automoviles` VALUES (12345678,'JJJ-123','A','Es un sedan'),(23456789,'PPP-456','B','Es un hatchback'),(98765432,'SVN-987','C','Es un mini');
+INSERT INTO `automoviles` VALUES (12345678,'JJJ-123','A','Es un sedan',NULL),(23456789,'PPP-456','B','Es un hatchback',NULL),(98765432,'SVN-987','C','Es un mini',NULL);
 /*!40000 ALTER TABLE `automoviles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -142,7 +143,7 @@ DROP TABLE IF EXISTS `contrato`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `contrato` (
   `CI` int NOT NULL,
-  `tipo` varchar(50) DEFAULT NULL,
+  `tipo` varchar(50) NOT NULL,
   `fecha_contrato` date NOT NULL,
   `horas_efectuadas` int NOT NULL,
   `horas_reservadas` int NOT NULL,
@@ -186,30 +187,6 @@ LOCK TABLES `cuenta` WRITE;
 /*!40000 ALTER TABLE `cuenta` DISABLE KEYS */;
 /*!40000 ALTER TABLE `cuenta` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `fotos`
---
-
-DROP TABLE IF EXISTS `fotos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `fotos` (
-  `matricula` varchar(50) NOT NULL,
-  `url` varchar(200) NOT NULL,
-  PRIMARY KEY (`matricula`),
-  CONSTRAINT `fotos_ibfk_1` FOREIGN KEY (`matricula`) REFERENCES `automoviles` (`matricula`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `fotos`
---
-
-LOCK TABLES `fotos` WRITE;
-/*!40000 ALTER TABLE `fotos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `fotos` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -220,4 +197,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-17 19:42:10
+-- Dump completed on 2021-04-20 12:28:44
