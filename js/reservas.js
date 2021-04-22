e639f0a177c7342224cf6a454b150cbfb2987281
SvenQuiring <svenpqiep@gmail.com>
2021-04-22 16:33:43 -0300

    Hora y dia.

--- js/reservas.js
+++ js/reservas.js
@@ -1,17 +1,26 @@
-
-
 function revisarHoras(dia) {
+    console.log(dia);
     $.ajax({
         url: "Reservas.php",
         type: "post",
         data: { dia:dia },
         success: function (html) {
-          document.getElementById(".col2").innerHTML = html;
+            $(".col2").html(html);
         },
     });
 }
 
+//  function horasSeguidas(hrsel) {
+//      $.ajax({
+//          url: "Reservas.php",
+//          type: "post",
+//          data: {hrsel:hrsel},
+//          success: function (html) {
+//              $(".col2").html(html);
+//          },
+//      });
+//  }
 
-$( document ).ready(function() {
-    $(".col2").load('Reservas.php');
-});
+//  $( document ).ready(function() {
+//        $(".col2").load('Reservas.php');
+//  });
