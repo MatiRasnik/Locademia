ee2ec4b0f5a92fc0fa359a7f50b6b8cc5c042872
ElTomy <tomassosa1303@gmail.com>
2021-04-22 14:47:06 -0300

    Avances register funcion

--- servidor.php
+++ servidor.php
@@ -20,15 +20,67 @@ class servidor{
         $stmts = $conn->prepare($sql);
 
         $stmts->bind_param("ssi",$usuario, $pass, $ci)
-        }
     }
 
     function login(){
 
     }
+/*
+    switch($_POST['Funcion']){
+        case "1":
+            $fun = $this->crearUsuario($_POST['usuario'],$_POST['pass'],$_POST['ci']);
+            break;
+        case "2":
+            $fun = $this->login($_POST['usuario'],$_POST['pass']);
+            break;
+        case "3":
+            break;
+    }
+    function crearUsuario($usuario, $pass, $ci){
+        $respuesta;
+        $conn = $this->conectar();
+        if(isset($_POST['ci'])){
+            if(isset($_POST['usuario']) && isset($_POST['pass'])){
+                else{
+                    $sql = "CALL crearUsuario(?,?,?)";
+                    $stmts = $conn->prepare($sql);
+                    $stmts->bind_param("ssi",$usuario, $pass, $ci);
+                    if($stmts->execute()){
+                        $stmts->store_result()
+                        $stmts->bind_result($result)
+                        if($result = 1){
+                            $respuesta = "3";
+                        }else{
+                            $respuesta = "4";
+                        }
+                    }else{
+                        $respuesta = "5";
+                    }
+                }
+            }else{
+                $sql = "CALL ComprobarCI(?)";
+                $stmts = $conn->prepare($sql);
+                $stmts->bind_param("i",$ci);
+                if($stmts->execute()){
+                    $stmts->store_result()
+                        $stmts->bind_result($result)
+                        if($result = 1){
+                            $respuesta = "1";
+                        }else{
+                            $respuesta = "2";
+                        }
+                }else{
+                    $respuesta = "5";
+                }
+            }
+        }else{
+            $respuesta = "2";
+        }
+        $conn = $this->close();
+        return $respuesta;
+    }
 
 
-
-
+*/
 }
 ?>
