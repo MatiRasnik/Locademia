974f81341e1e60a7044fa773159f5026b5e8db81
MatiRasnik <MatiRasnik@users.noreply.github.com>
2021-04-26 17:58:13 -0300

    asdd

--- servidor.php
+++ servidor.php
@@ -66,50 +66,38 @@ class servidor{
         return $log;
         }
     
-
-/*
-    switch($_POST['Funcion']){
-        case "1":
-            $fun = $this->crearUsuario($_POST['usuario'],$_POST['pass'],$_POST['ci']);
-            break;
-        case "2":
-            $fun = $this->login($_POST['usuario'],$_POST['pass']);
-            break;
-        case "3":
-            break;
-    }
-
-    function crearUsuario($usuario, $pass, $ci){
+        function crearUsuario($usuario, $pass, $ci){
         
-        $respuesta;
-        $conn = $this->conectar();
-        if(isset($_POST['ci'])){
-            if(isset($_POST['usuario']) && isset($_POST['pass'])){
-                else{
-                    $sql = "CALL crearUsuario(?,?,?)";
+            $respuesta;
+            $conn = $this->conectar();
+            if(isset($_POST['ci'])){
+                if(isset($_POST['usuario']) && isset($_POST['pass'])){
+                    else{
+                        $sql = "CALL crearUsuario(?,?,?)";
+                        $stmts = $conn->prepare($sql);
+                        $stmts->bind_param("ssi",$usuario, $pass, $ci);
+                        if($stmts->execute()){
+                            $respuesta = 3;
+                        }else{
+                            $respuesta = 4;
+                        }
+                    }
+                }else{
+                    $sql = "CALL ComprobarCI(?)";
                     $stmts = $conn->prepare($sql);
-                    $stmts->bind_param("ssi",$usuario, $pass, $ci);
+                    $stmts->bind_param("i",$ci);
                     if($stmts->execute()){
-                        $respuesta = 3;
+                        $respuesta = 1;
                     }else{
-                        $respuesta = 4;
+                        $respuesta = 2;
                     }
                 }
             }else{
-                $sql = "CALL ComprobarCI(?)";
-                $stmts = $conn->prepare($sql);
-                $stmts->bind_param("i",$ci);
-                if($stmts->execute()){
-                    $respuesta = 1;
-                }else{
-                    $respuesta = 2;
-                }
+                $respuesta = 5;
             }
-        }else{
-            $respuesta = 5;
+            $conn = $this->close();
+            return $respuesta;
         }
-        $conn = $this->close();
-        return $respuesta;
     }
-}
-?>
+    ?>
+  
