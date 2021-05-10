a7ea794ffc06504257a29fae1818ec0a58d3c316
MatiRasnik <MatiRasnik@users.noreply.github.com>
2021-05-10 15:45:15 -0300

    autos

---
+++ Form/CerrarSesion.php
@@ -0,0 +1,7 @@
+<?php
+    $borrado = "borrado";
+        echo "<scripo>console.log('destruir')</script>";  
+        echo $borrado;
+        return $borrado;
+        //session_destroy();
+?>
--- js/fuctions.js
+++ js/fuctions.js
@@ -34,9 +34,14 @@ function traigoCoches(){
 }
 
 function armoAutos(tipocar){
+  mat = sessionStorage.getItem('matricula');
   txt = tipocar;
   log2 = JSON.parse(txt);
-  var tipo;
+
+  if(sessionStorage.getItem('matricula').length <= 4){
+    //no tiene auto
+    var tipo;
+
   if(log2[1]["tipo"] == "A"){
     tipo = "SEDAN";
   }else{
@@ -46,13 +51,31 @@ function armoAutos(tipocar){
       tipo = "MINI";
     }
   }
- var autos = "  <div class='vector-wrapper'> \n <img class='vector' src='../img/Main-Vector.svg' alt='' /> \n </div> \n <div class='tipo-wrapper-grid'> \n <div class='tipo-wrapper'>\n  <h2>Seleccione el Modelo del Vehiculo:</h2> \n  <p>" + tipo + "</p> \n <select name='autos' id='autos' onchange=fotoAuto(this);>";
- for (var i = 0; i < log2.length; i++) {
-  autos = autos + "  \n <option id='auto-select' value='"+log2[i]["desc"]+"'>"+log2[i]["desc"]+"</option> ";
-}
-  autos = autos + "</select> \n <button onclick='agenda()'>Seleccionar</button> \n </div> \n <div class='foto-vehiculo' id= 'foto-vehiculo'>";
-  autos = autos + " \n <img src='../img/"+log2[0]["url"]+"' alt='' />";
-  autos = autos + "\n </div> \n </div> \n <div class='vector-wrapper-rotate'> \n <img class='vector' src='../img/Main-Vector.svg' alt='' /> \n </div> ";
+    var autos = "  <div class='vector-wrapper'> \n <img class='vector' src='../img/Main-Vector.svg' alt='' /> \n </div> \n <div class='tipo-wrapper-grid'> \n <div class='tipo-wrapper'>\n  <h2>Seleccione el Modelo del Vehiculo:</h2> \n  <p>" + tipo + "</p> \n <select name='autos' id='autos' onchange=fotoAuto(this);>";
+    
+    for (var i = 0; i < log2.length; i++) {
+        autos = autos + "  \n <option id='auto-select' value='"+log2[i]["desc"]+"'>"+log2[i]["desc"]+"</option> ";
+    }
+
+    autos = autos + "</select> \n <button onclick='agenda()'>Seleccionar</button> \n </div> \n <div class='foto-vehiculo' id= 'foto-vehiculo'>";
+    autos = autos + " \n <img src='../img/"+log2[0]["url"]+"' alt='' />";
+    autos = autos + "\n </div> \n </div> \n <div class='vector-wrapper-rotate'> \n <img class='vector' src='../img/Main-Vector.svg' alt='' /> \n </div> ";
+
+  }else{
+    //tiene auto
+    for (var i = 0; i < log2.length; i++) {
+      if(log2[i]["matricula"] == mat){
+        var coche = log2[i]["desc"];
+        var imgAuto = log2[i]["url"];
+      }
+   }
+    var autos = "  <div class='vector-wrapper'> \n <img class='vector' src='../img/Main-Vector.svg' alt='' /> \n </div> \n <div class='tipo-wrapper-grid'> \n <div class='tipo-wrapper'>\n  <h2>Su Vehiculo:</h2> \n  <p>" + coche + "</p>";
+
+    autos = autos + "\n </div> \n <div class='foto-vehiculo' id= 'foto-vehiculo'>";
+    autos = autos + " \n <img src='../img/"+imgAuto+"' alt='' />";
+    autos = autos + "\n </div> \n </div> \n <div class='vector-wrapper-rotate'> \n <img class='vector' src='../img/Main-Vector.svg' alt='' /> \n </div> ";
+  }
+  
   document.getElementById("phpcoche").innerHTML = autos;
 }
 
@@ -88,11 +111,18 @@ function armoHoras(horas){
       $(".horas").html(html);
     }
   })
-  for (var i = 0; i < log2.length; i++) {
-    console.log(log2[i]["id"]);
-    console.log(log2[i]["ci"]);
-    console.log(log2[i]["dia"]);
-    console.log(log2[i]["horaComienzo"]);
-    console.log(log2[i]["horaFin"]);
-  }
+}
+
+function cerrarSesion(){
+  alert("asd");
+  $.ajax({
+    url: "../Form/CerrarSesion.php",
+    type: "post",
+    data: {},
+    succes: function (asd) {
+      alert(asd);
+      alert("borrado");
+      //storage.removeItem(keyName);
+    }
+  })
 }
