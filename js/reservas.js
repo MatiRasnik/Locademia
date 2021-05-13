cacb644a9d05d6c47b09870d483f26f7ff29d17a
SvenQuiring <svenpqiep@gmail.com>
2021-05-13 19:49:14 -0300

    Funciona el sabado?

--- js/reservas.js
+++ js/reservas.js
@@ -86,3 +86,70 @@ function horasSeguidas(hrsel) {
         }
     }
 }
+
+function revisarSabado(id, hora) {
+    horaL = hora;
+    arri.length=0;
+    diaG = id;
+    $.ajax({
+        url: "Resabado.php",
+        type: "post",
+        data: { dia: diaG, Horas2:Horas2 },
+        success: function (html) {
+            $(".horas").html(html);
+            hrReservadas();
+        }
+    });
+}
+
+function horasSabado(hrsel) {
+    if(arri.indexOf(hrsel) == -1) {
+        arri.push(hrsel);
+    } else {
+        arri.splice(arri.indexOf(hrsel), 1);
+    }
+
+    var reser = [];
+    for(var i=0;i<asd2.length;i++) {
+        reser.push(parseInt(asd2[i]));
+    }
+
+    for(var a=7;a<=10;a++) {
+        if(a+1 < arri[0] || a-1 > arri[0] || a+1 < arri[1] || a-1 > arri[1]) {
+            if(reser.indexOf(a) != -1) {
+                document.getElementById(a).disabled = true;
+                document.getElementById("H"+a).style.backgroundColor = "#db5e5e";
+            } else {
+                document.getElementById(a).disabled = true;
+                document.getElementById("H"+a).style.backgroundColor = "lightgrey";
+            }
+        } else if(horaL <= a) {
+            if(reser.indexOf(a) != -1) {
+                document.getElementById(a).disabled = true;
+                document.getElementById("H"+a).style.backgroundColor = "#db5e5e";
+            } else {
+                document.getElementById(a).disabled = false;
+                document.getElementById("H"+a).style.backgroundColor = "white";
+            }
+        }
+    }
+    for(var a=11;a<=13;a++) {
+        if(a+1 < arri[0] || a-1 > arri[0] || a+1 < arri[1] || a-1 > arri[1]) {
+            if(reser.indexOf(a) != -1) {
+                document.getElementById(a).disabled = true;
+                document.getElementById("H"+a).style.backgroundColor = "#db5e5e";
+            } else {
+                document.getElementById(a).disabled = true;
+                document.getElementById("H"+a).style.backgroundColor = "lightgrey";
+            }
+        } else if(horaL <= a) {
+            if(reser.indexOf(a) != -1) {
+                document.getElementById(a).disabled = true;
+                document.getElementById("H"+a).style.backgroundColor = "#db5e5e";
+            } else {
+                document.getElementById(a).disabled = false;
+                document.getElementById("H"+a).style.backgroundColor = "white";
+            }
+        }
+    }
+}
