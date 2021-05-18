7b945b84f07cfe25e843b54fbf41b93ba4d82e6a
SvenQuiring <svenpqiep@gmail.com>
2021-05-18 12:46:52 -0300

    UWUWUWUWUUWUUWUWUWU

--- Form/resaBD.php
+++ Form/resaBD.php
@@ -19,10 +19,28 @@ if(isset($_POST['arri']) && isset($_POST['diaG']) && isset($_POST['tipo'])) {
                 $server->agendar($ci, $matricula, $dia, $hora_inicio, $hora_fin);
             }
         }
+    } else if($_POST['tipo'] == 'opc2') {
+        for($i=0;$i<$horas_restantes;$i++) {
+            $dia = date('Y-m-d', mktime(0, 0, 0, $revers[1], $revers[0]+$i*7, $revers[2]));
+            sort($irra, SORT_NATURAL);
+            for($j=0;$j<count($irra);$j++) {
+                $hora_inicio = $irra[$j];
+                $hora_fin =$hora_inicio + 1;
+                $server->agendar($ci, $matricula, $dia, $hora_inicio, $hora_fin);
+            }
+        }
+    } else if($_POST['tipo'] == 'opc3') {
+        $dia = date('Y-m-d', mktime(0, 0, 0, $revers[1], $revers[0], $revers[2]));
+        sort($irra, SORT_NATURAL);
+        for($j=0;$j<count($irra);$j++) {
+            $hora_inicio = $irra[$j];
+            $hora_fin =$hora_inicio + 1;
+            $server->agendar($ci, $matricula, $dia, $hora_inicio, $hora_fin);
+        }
+    } else {
+        echo 'MANCO 2';
     }
-
 } else {
     echo "MANCOOOO";
 }
-
 ?>
