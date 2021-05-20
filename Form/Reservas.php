8d48cc9b5a2d75b76662a592e379e64bf06ca261
MatiRasnik <MatiRasnik@users.noreply.github.com>
2021-05-19 22:23:21 -0300

    &&

--- Form/Reservas.php
+++ Form/Reservas.php
@@ -39,7 +39,7 @@ echo "<div class='col-grid'>
             </div>";
 if(isset($_POST['Horas2'])){
     for($i=7;$i<=12;$i++) {
-        if(date("H", mktime($i)) < $hora && date("j") >= $arr[0] && date("n") >= $arr[1] && date("Y") >= $arr[2] || $hora < 3) {
+        if(date("H", mktime($i)) < $hora && date("j") >= $arr[0] && date("n") >= $arr[1] && date("Y") >= $arr[2] && $hora < 3) {
             for($j=0;$j<count($horas);$j++) {
                 if($log2[$horas[$j]]["horaComienzo"] == $i || $log2[$horas[$j]]["horaComienzo"] == $i) {
                     echo "<label class='horario' id='H" . $i . "' style='background-color: #db5e5e; color: white'> <input type='checkbox' id=" . $i . " name='horario1' disabled value=" . date('H:i', mktime($i, 00)) . "> <label>" . date('H:i', mktime($i, 00)) . " hs </label> </label>";
@@ -63,7 +63,7 @@ if(isset($_POST['Horas2'])){
     }
 }else{
     for($i=7;$i<=12;$i++) {
-        if(date("H", mktime($i)) < $hora && date("j") >= $arr[0] && date("n") >= $arr[1] && date("Y") >= $arr[2] || $hora < 3) {
+        if(date("H", mktime($i)) < $hora && date("j") >= $arr[0] && date("n") >= $arr[1] && date("Y") >= $arr[2] && $hora < 3) {
                     echo "<label class='horario' id='H" . $i . "' style='background-color: lightgrey;'> <input type='checkbox' id=" . $i . " name='horario1' disabled value=" . date('H:i', mktime($i, 00)) . "> <label>" . date('H:i', mktime($i, 00)) . " hs </label> </label>";
         } else {
             echo "<label class='horario' id='H" . $i . "'> <input type='checkbox' id=" . $i . " name='horario1' onchange='horasSeguidas(" . $i . ")'> <label>" . date('H:i', mktime($i, 00)) . " hs </label> </label>";
@@ -78,7 +78,7 @@ echo "<div class='col-flex'> <div class='col2'> <div class='Titulo'> <h1>Vespert
 
 if(isset($_POST['Horas2'])){
     for($i=14;$i<=19;$i++) {
-        if(date("H", mktime($i)) < $hora && date("j") >= $arr[0] && date("n") >= $arr[1] && date("Y") >= $arr[2] || $hora < 3) {
+        if(date("H", mktime($i)) < $hora && date("j") >= $arr[0] && date("n") >= $arr[1] && date("Y") >= $arr[2] && $hora < 3) {
             for($j=0;$j<count($horas);$j++) {
                 if($log2[$horas[$j]]["horaComienzo"] == $i || $log2[$horas[$j]]["horaComienzo"] == $i) {
                     echo "<label class='horario' id='H" . $i . "' style='background-color: #db5e5e; color: white'> <input type='checkbox' id=" . $i . " name='horario1' disabled value=" . date('H:i', mktime($i, 00)) . "> <label>" . date('H:i', mktime($i, 00)) . " hs </label> </label>";
@@ -102,7 +102,7 @@ if(isset($_POST['Horas2'])){
     }
 }else{
     for($i=14;$i<=19;$i++) {
-        if(date("H", mktime($i)) < $hora && date("j") >= $arr[0] && date("n") >= $arr[1] && date("Y") >= $arr[2] || $hora < 3) {
+        if(date("H", mktime($i)) < $hora && date("j") >= $arr[0] && date("n") >= $arr[1] && date("Y") >= $arr[2] && $hora < 3) {
                     echo "<label class='horario' id='H" . $i . "' style='background-color: lightgrey;'> <input type='checkbox' id=" . $i . " name='horario1' disabled value=" . date('H:i', mktime($i, 00)) . "> <label>" . date('H:i', mktime($i, 00)) . " hs </label> </label>";
         } else {
             echo "<label class='horario' id='H" . $i . "'> <input type='checkbox' id=" . $i . " name='horario1' onchange='horasSeguidas(" . $i . ")'> <label>" . date('H:i', mktime($i, 00)) . " hs </label> </label>";
